f124076d14a2ab8fd1d6d16cbbbb16632de214ad
Eric Cornelissen <ericornelissen@gmail.com>
2018-09-07 10:16:31 +0200

    Complete test suite for PinnedTabsState

--- spec/state-spec.js
+++ spec/state-spec.js
@@ -1,8 +1,14 @@
 'use babel';
 
+import { spy } from 'sinon';
+import sinonChai from 'sinon-chai';
+
 import { testquire } from 'atom-coverage';
+import { Stats } from 'fs';
 const PinnedTabsState = testquire('state.js');
 
+
+Chai.use(sinonChai);
 
 describe('PinnedTabsState', function() {
 
@@ -93,25 +99,167 @@ describe('PinnedTabsState', function() {
 
   describe('::addPane()', () => {
 
+    let state;
+
+    beforeEach(() => {
+      state = new PinnedTabsState();
+    });
+
+    it('adds a new pane to the state', () => {
+      state.addPane(42);
+      expect(state).to.have.property(42);
+      expect(state.indices).to.include(42);
+    });
+
+    it('does nothing if the pane is already in the state', () => {
+      state[36] = [];
+      state.indices = [36];
+
+      state.addPane(36);
+      expect(state).to.have.property(36);
+      expect(state.indices).to.have.length(1);
+    });
+
   });
 
   describe('::addPaneItem()', () => {
+
+    let state;
+
+    beforeEach(() => {
+      state = new PinnedTabsState();
+    });
+
+    it('adds a new pane item to the state', () => {
+      state.addPaneItem(42, {getTitle: () => 'foobar'});
+      expect(state).to.have.property(42);
+      expect(state[42]).to.have.length(1);
+    });
+
+    it('does nothing if the pane item is already in the state', () => {
+      state[36] = [{id: 'Hello world!'}];
+
+      state.addPaneItem(34, {getTitle: () => 'Hello world!'});
+      expect(state[36]).to.have.length(1);
+    });
 
   });
 
   describe('::forEach()', () => {
 
+    let callback;
+
+    beforeEach(() => {
+      callback = spy();
+    });
+
+    it('does nothing if there are no pinned items', () => {
+      let state = new PinnedTabsState();
+
+      state.forEach(callback);
+      expect(callback).not.to.be.called;
+    });
+
+    it('iterates over each item in one pane', () => {
+      let state = new PinnedTabsState();
+      state.indices = [1];
+      state[1] = [{id: 'foo'}];
+
+      state.forEach(callback);
+      expect(callback).to.be.calledOnce;
+    });
+
+    it('iterates over each item in all panes', () => {
+      let state = new PinnedTabsState();
+      state.indices = [1, 2];
+      state[1] = [{id: 'foo'}];
+      state[2] = [{id: 'bar'}];
+
+      state.forEach(callback);
+      expect(callback).to.be.calledTwice;
+    });
+
   });
 
   describe('::movePaneItem()', () => {
+
+    let state;
+
+    beforeEach(() => {
+      state = new PinnedTabsState();
+    });
+
+    it('updates the state if a pinned pane item moved', () => {
+      state[42] = [{id: 'foo'}, {id: 'bar'}];
+
+      state.movePaneItem(42, {getTitle: () => 'foo'}, 1);
+      expect(state[42]).to.deep.equal([{id: 'bar'}, {id: 'foo'}]);
+    });
+
+    it('does nothing if the item was not actually moved', () => {
+      state[36] = [{id: 'foo'}, {id: 'bar'}];
+
+      state.movePaneItem(36, {getTitle: () => 'foo'}, 0);
+      expect(state[36]).to.deep.equal([{id: 'foo'}, {id: 'bar'}]);
+    });
 
   });
 
   describe('::removePane()', () => {
 
+    let state;
+
+    beforeEach(() => {
+      state = new PinnedTabsState();
+    });
+
+    it('removes a pane from the state', () => {
+      state.indices = [42];
+      state[42] = [{id: 'foobar'}];
+
+      state.removePane(42);
+      expect(state).not.to.have.property(42);
+      expect(state.indices).to.have.length(0);
+    });
+
+    it('does nothing if the pane was not in the state', () => {
+      state.indices = [42];
+      state[42] = [{id: 'foobar'}];
+
+      state.removePane(36);
+      expect(state).to.have.property(42);
+      expect(state.indices).to.deep.equal([42]);
+    });
+
   });
 
   describe('::removePaneItem()', () => {
+
+    let state;
+
+    beforeEach(() => {
+      state = new PinnedTabsState();
+    });
+
+    it('removes a pane item from the state', () => {
+      state[42] = [{id: 'foo', subscriptions: {dispose: spy()}}];
+
+      state.removePaneItem(42, {getTitle: () => 'foo'});
+      expect(state[42]).to.be.empty;
+    });
+
+    it('does nothing if the pane item was not in the state', () => {
+      state.removePaneItem(42, 'foo');
+      expect(state[42]).not.to.exist;
+    });
+
+    it('disposes the item descriptions', () => {
+      let subscriptionsDisposeSpy = spy();
+      state[42] = [{id: 'foo', subscriptions: {dispose: subscriptionsDisposeSpy}}];
+
+      state.removePaneItem(42, {getTitle: () => 'foo'});
+      expect(subscriptionsDisposeSpy).to.have.been.called;
+    });
 
   });
 
